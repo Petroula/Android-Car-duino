d000eb188f2a52b400ad991eb8618b20d0cc4a97
davidkron <david_jensen3@hotmail.com>
2015-04-30 04:06:14 +0200

    Minor tweaks

--- Include/imageprocessor/roadfinder.hpp
+++ Include/imageprocessor/roadfinder.hpp
@@ -1,6 +1,6 @@
 #include "line.hpp"
 #include <queue>
-#include "roadfragmentbuilder2.hpp"
+#include "roadbuilder.hpp"
 #include <numeric>
 #include "lightnormalizer.hpp"
 #include <memory>
@@ -10,73 +10,49 @@ namespace Autodrive
     class roadfinder
     {
         typedef cv::Point_ < float > POINT;
-        const float PI = Mathf::PI;
-        const float PI_2 = Mathf::PI_2;
-
-        roadfragmentbuilder2::Road road2;
-
-        int canny_thresh1;
-        int canny_thresh2;
+        roadbuilder::Road road;
 
     public:
 
-        int updateFrequency;
-        int updateCounter;
-
-        float getWay(float f)
-        {
-            if (f < 0.f)
-                return -1;
-            if (f == 0.f)
-                return 0;
-            if (f > 0.f)
-                return 1;
-            throw;
-        }
-
         POINT start_center;
-        std::unique_ptr<roadfragmentbuilder2> roadbuilder2 = nullptr;
+        std::unique_ptr<roadbuilder> roadBuilder = nullptr;
 
         cv::Mat draw2(const cv::Mat& cannied)
         {
             cv::Mat colorCopy;
             cv::cvtColor(cannied, colorCopy, CV_GRAY2RGB);
 
-            for (unsigned int i = 0; i < road2.points.size() -1; i++)
+            for (unsigned int i = 0; i < road.points.size() -1; i++)
             {
-                linef(road2.points[i], road2.points[i + 1]).draw(colorCopy,cv::Scalar(255,0,0),1);
+                linef(road.points[i], road.points[i + 1]).draw(colorCopy,cv::Scalar(255,0,0),1);
             }
 
-            linef(roadbuilder2->last_start, roadbuilder2->last_start + POINT(8,-15)).draw(colorCopy,cv::Scalar(0,255,255),1);
+            linef(roadBuilder->last_start, roadBuilder->last_start + POINT(8, -15)).draw(colorCopy, cv::Scalar(0, 255, 255), 1);
             linef(start_center, POINT(start_center.x, 0)).draw(colorCopy);
             
             return colorCopy;
-            /*cv::resize(colorCopy, colorCopy, colorCopy.size() * 3);//resize image
-            cv::namedWindow("mainwindow", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-            cv::imshow("mainwindow", colorCopy);
-            cv::waitKey(10); // waits to display frame*/
         }
 
-        void build2(const cv::Mat& cannied, POINT start_point, int _updateFrequency)
+        void build2(const cv::Mat& cannied, POINT start_point, int _updateFrequency) 
         {
-            updateFrequency = _updateFrequency;
-            roadbuilder2 = make_unique<roadfragmentbuilder2>(cannied, start_point);
+            roadBuilder = make_unique<roadbuilder>(cannied, start_point);
+            road = roadBuilder->build2(cannied, 25);
         }
 
         command update2(cv::Mat& cannied,cv::Mat& drawMat)
         {
-            road2 = roadbuilder2->build2(cannied, 50);
+            road = roadBuilder->build2(cannied, 25);
             
             drawMat = draw2(cannied);
             command cmd;
 
-            int mid = int(road2.points.size() / 2.5);
+            int mid = int(road.points.size() / 2.5);
             
-            if (road2.points.size() < 5 || abs(road2.getMeanAngle() - Mathf::PI_2) > Mathf::PI_2)
+            if (road.points.size() < 5 || abs(road.getMeanAngle() - Mathf::PI_2) > Mathf::PI_2)
                 cmd.setSpeed(60);
             else
             {
-                int degrees = Mathf::toDegrees(road2.getMeanAngle()) - 90;
+                int degrees = Mathf::toDegrees(road.getMeanAngle()) - 90;
                 degrees = int((degrees / 50.f) * 25);
 
                 degrees = std::min(degrees, 25);
