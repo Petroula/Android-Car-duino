02839ddc68a069d96ba08fd690573f95dc13ed3d
davidkron <david_jensen3@hotmail.com>
2015-05-03 04:41:52 +0200

    Added some more c++ warnings to incrase compat with autodriveSim

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_OPTIM := release
 APP_STL := gnustl_shared
-APP_CPPFLAGS := -frtti -fexceptions -std=c++11 -O3
+APP_CPPFLAGS := -frtti -fexceptions -std=c++11 -O3 -Wno-deprecated -Wall -Wextra -Wpointer-arith -Wwrite-strings -Wpacked -pipe
 NDK_TOOLCHAIN_VERSION := 4.8
 APP_ABI := mips armeabi armeabi-v7a
