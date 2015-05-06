9cdf653ec94228c4cd2e42d8add8d8282f3f1142
Petroula Theodoridou <gustheodpe@student.gu.se>
2015-05-06 14:55:13 +0200

    emulator error fix

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,4 +2,4 @@ APP_OPTIM := release
 APP_STL := gnustl_shared
 APP_CPPFLAGS := -frtti -fexceptions -std=c++11 -O3 -Wno-deprecated -Wall -Wextra -Wpointer-arith -Wwrite-strings -Wpacked -pipe
 NDK_TOOLCHAIN_VERSION := clang
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a x86
