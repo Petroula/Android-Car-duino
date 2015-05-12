42ed8ac727662a067d17d3b115ffb1beaf774b7d
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-05-12 15:58:33 +0200

    Re-added USEFUL getters.

--- app/src/main/java/pegasus/bluetootharduino/Autodrive.java
+++ app/src/main/java/pegasus/bluetootharduino/Autodrive.java
@@ -19,7 +19,7 @@ public class Autodrive
 
 /*----- SENSORDATA -----*/
     public static native void setImage(long matAddrRgba);
-    public static native void setUltraSound(int sensor, int value);
+    public static native void setUltrasound(int sensor, int value);
     public static native int getUltrasoundSensor(int sensor);
     public static native void setInfrared(int sensor, int value);
     public static native int getInfraredSensor(int sensor);
