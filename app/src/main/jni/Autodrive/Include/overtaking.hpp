cb75e681bd5ef765e8576e03d1fc18b6f86aaebb
Ibtissam <ik@karouach.se>
2015-05-25 06:12:43 +0200

    Started integrating overtaking logic into the car from simulator. Stop the car once it sees the object at specified distance.

--- app/src/main/jni/Autodrive/Include/overtaking.hpp
+++ app/src/main/jni/Autodrive/Include/overtaking.hpp
@@ -1,6 +1,15 @@
+#include "sensordata.hpp"
+
 namespace Autodrive {
     namespace Overtaking {
+        int usFront = SensorData::ultrasound.front;
+        int objectDistance = 50;
+
         command run(command lastCommand) {
+            if (usFront > 0 && usFront < objectDistance) {
+                if (overtaking < 1) overtaking = lastCommand.setSpeed(0);
+            }
+
              return lastCommand;
         }
     }
