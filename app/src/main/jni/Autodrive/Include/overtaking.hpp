b6150ffc804875254d17016ad564abf2c3865d33
Ibtissam <ik@karouach.se>
2015-05-25 06:15:42 +0200

    Fixed boolean error.

--- app/src/main/jni/Autodrive/Include/overtaking.hpp
+++ app/src/main/jni/Autodrive/Include/overtaking.hpp
@@ -4,10 +4,15 @@ namespace Autodrive {
     namespace Overtaking {
         int usFront = SensorData::ultrasound.front;
         int objectDistance = 50;
+        bool overtaking = false;
 
         command run(command lastCommand) {
             if (usFront > 0 && usFront < objectDistance) {
-                if (overtaking < 1) overtaking = lastCommand.setSpeed(0);
+                if (! overtaking) overtaking = true;
+            }
+
+            if (overtaking) {
+                lastCommand.setSpeed(0);
             }
 
              return lastCommand;
