7957d989a82b3f2c64d5902a01d79007389cdbe0
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-05-25 06:24:41 +0200

    Fix overtaking variable value.

--- app/src/main/jni/Autodrive/Include/overtaking.hpp
+++ app/src/main/jni/Autodrive/Include/overtaking.hpp
@@ -5,7 +5,7 @@ namespace Autodrive {
     namespace Overtaking {
         int usFront = SensorData::ultrasound.front;
         int objectDistance = 50;
-        bool overtaking = false;
+        float overtaking = 0;
 
         command run(command lastCommand) {
             if (usFront > 0 && usFront < objectDistance) {
