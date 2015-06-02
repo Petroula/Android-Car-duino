be763b3d3bd9d0739a48f7c2279dd4c56ed28b89
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-06-02 22:46:42 +0200

    Completed logic for overtaking using dash line detection.

--- app/src/main/jni/Autodrive/Include/overtaking.hpp
+++ app/src/main/jni/Autodrive/Include/overtaking.hpp
@@ -9,38 +9,13 @@ using namespace cv;
 namespace Autodrive {
     namespace Overtaking {
         bool overtaking = false;
-        bool obstacleMet = false;
         bool finaliseOvertaking = false;
-        bool stop = false;
-        bool goToLeftLane = false;
-        bool goToRightLane = false;
-        bool oomph = false;
-        bool dashed = false;
-
-//        sensor usFront, irFrontRight, irRearRight;
+        bool turnLeft = false;
+        bool turnRight = false;
+        int oomph = 0;
+        bool obstacleMet = false;
 
         command run(command lastCommand, Mat* mat) {
-//            lastCommand.setSpeed(0);
-
-//            usFront.set(SensorData::ultrasound.front);
-//            irFrontRight.set(SensorData::infrared.frontright);
-//            irRearRight.set(SensorData::infrared.rearright);
-
-//            char usFrontText[25], irFrontRightText[25], irRearRightText[25];
-//            sprintf(usFrontText, "usFront: %d",  usFront.value);
-//            sprintf(irFrontRightText, "irFrontRight: %d",  irFrontRight.value);
-//            sprintf(irRearRightText, "irRearRight: %d", irRearRight.value);
-//            cv::putText(*mat, usFrontText, POINT(50.f, mat->size().height / 6.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//            cv::putText(*mat, irFrontRightText, POINT(50.f, mat->size().height / 4.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//            cv::putText(*mat, irRearRightText, POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-
-//            usFront.previous = usFront.value;
-//            irFrontRight.previous = irFrontRight.value;
-//            irRearRight.previous = irRearRight.value;
-
-//            return lastCommand;
-
-
             int usFront = SensorData::ultrasound.front;
             int irFrontRight = SensorData::infrared.frontright;
             int irRearRight = SensorData::infrared.rearright;
@@ -48,76 +23,79 @@ namespace Autodrive {
 
             if (usFront > 0 && usFront < objectDistance) {
                 if (! overtaking) overtaking = true;
-                if (! goToLeftLane) goToLeftLane = true;
+                if (! turnLeft) turnLeft = SensorData::encoderDistance();
             }
 
-            if (overtaking && ! finaliseOvertaking) {
-                if (! obstacleMet) {
-                    cv::putText(*mat, "overtaking", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-                }
-
-                if (goToLeftLane) lastCommand.setAngle(-1);
+            if (overtaking) {
+                if (turnLeft) lastCommand.setAngle(-1);
 
                 if (imageProcessor::dashedLineGaps() > 1) {
-                    goToLeftLane = false;
-                    dashed = true;
-//                    oomph = SensorData::encoderDistance();
+                    turnLeft = false;
+                    oomph = SensorData::encoderDistance();
                 }
 
-                if (dashed) cv::putText(*mat, "dashed", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+                if (SensorData::encoderDistance() - oomph < 40) {
+                    lastCommand.setAngle(0);
+                }
 
-//                if (SensorData::encoderDistance() - oomph < 30) {
-//                    ran = true;
-//                    cv::putText(*mat, "dashed", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//                    lastCommand.setAngle(0);
-//                }
-//
-//                if (ran) cv::putText(*mat, "runs", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+                if (irRearRight > 5 && irRearRight < 5) if (! obstacleMet) obstacleMet = true;
 
+                if (obstacleMet) {
+                    if (! irFrontRight) {
+                        obstacleMet = false;
+                        oomph = 0;
 
+                        if (! finaliseOvertaking) finaliseOvertaking = true;
+                        if (! turnRight) turnRight = true;
+                    }
+                }
 
-//                if (irRearRight > 5 && irRearRight < 10) if (! obstacleMet) obstacleMet = true;
-//
-//                if (obstacleMet) {
-//                    cv::putText(*mat, "obstacle met", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//
-//                    if (irFrontRight > 15 || ! irFrontRight) {
-//                        overtaking = 0;
-//                        obstacleMet = false;
-//
-//                        if (! finaliseOvertaking) finaliseOvertaking = SensorData::encoderDistance();
-//                    }
-//                }
+                if (finaliseOvertaking) {
+                    if (turnRight) lastCommand.setAngle(1);
+
+                    if (imageProcessor::dashedLineGaps() > 1) {
+                        turnRight = false;
+                        oomph = SensorData::encoderDistance();
+                    }
+
+                    if (SensorData::encoderDistance() - oomph < 15) {
+                        lastCommand.setAngle(0);
+                    }
+
+                    if (SensorData::encoderDistance() - oomph > 15) {
+                        turnRight = false;
+                        finaliseOvertaking = false;
+                        overtaking = false;
+                        oomph = 0;
+                    }
+                }
             }
 
-//            if (finaliseOvertaking) {
-////                lastCommand.setSpeed(slowSpeed);
-//
-//                cv::putText(*mat, "back to lane", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//
-//                if (! imageProcessor::isRightLane()) {
-//                    lastCommand.setAngle(1);
-//                } else if (imageProcessor::isLeftLane()) {
-//                    finaliseOvertaking = 0;
-//                }
-//            }
+            if (overtaking) {
+                cv::putText(*mat, "overtaking", POINT(50.f, mat->size().height / 4.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+            }
 
-//            if (stop) {
-//                lastCommand.setSpeed(0);
-//                cv::putText(*mat, "stop", POINT(50.f, mat->size().height / 1.5f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//            }
-//
-//            if (usFront > 0 && usFront < 15) lastCommand.setSpeed(0);
-//
-//            if (imageProcessor::isLeftLane()) {
-//                cv::putText(*mat, "left lane", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//            } else if (imageProcessor::isRightLane()) {
-//                cv::putText(*mat, "right lane", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//            } else {
-//                cv::putText(*mat, "not found", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-//            }
+            if (turnLeft) {
+                cv::putText(*mat, "turning left", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+            }
 
-            lastCommand.setSpeed(0.25);
+            if (turnRight) {
+                cv::putText(*mat, "turning right", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+            }
+
+            if (oomph) {
+                cv::putText(*mat, "oomph", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+            }
+
+            if (obstacleMet) {
+                cv::putText(*mat, "oomph", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+            }
+
+            if (finaliseOvertaking) {
+                cv::putText(*mat, "finalise overtaking", POINT(50.f, mat->size().height / 1.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+            }
+
+            lastCommand.setSpeed(0.26);
             return lastCommand;
         }
     }
