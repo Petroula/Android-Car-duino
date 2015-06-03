b1aef58cd3ce62caeffda07d89591e16728ca5e6
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-06-03 10:17:59 +0200

    Hard-coded lane switching. Added more flags for stricter, yet safer procedure.

--- app/src/main/jni/Autodrive/Include/overtaking.hpp
+++ app/src/main/jni/Autodrive/Include/overtaking.hpp
@@ -8,94 +8,123 @@ using namespace cv;
 
 namespace Autodrive {
     namespace Overtaking {
+        bool debugMode = true;
+
+        int usFront = 0;
+        int usFrontRight = 0;
+        int irFrontRight = 0;
+        int irRearRight = 0;
+        int distanceTravelled = 0;
+
         bool overtaking = false;
-        bool finaliseOvertaking = false;
-        bool turnLeft = false;
-        bool turnRight = false;
-        int oomph = 0;
+        int turnLeft = 0;
+        int turnLeftCalibration = 0;
+        int turnRight = 0;
+        int turnRightCalibration = 0;
+        bool turnRightCalibrationFinished = false;
+        int obstacleDistance = 70;
         bool obstacleMet = false;
+        bool obstaclePassed = false;
 
         command run(command lastCommand, Mat* mat) {
-            int usFront = SensorData::ultrasound.front;
-            int irFrontRight = SensorData::infrared.frontright;
-            int irRearRight = SensorData::infrared.rearright;
-            int objectDistance = 70;
+            usFront = SensorData::ultrasound.front;
+            usFrontRight = SensorData::ultrasound.frontright;
+            irFrontRight = SensorData::infrared.frontright;
+            irRearRight = SensorData::infrared.rearright;
+            distanceTravelled = SensorData::encoderDistance();
 
-            if (usFront > 0 && usFront < objectDistance) {
-                if (! overtaking) overtaking = true;
-                if (! turnLeft) turnLeft = true;
+            if (usFront > 0 && usFront < obstacleDistance) {
+                if (! overtaking) {
+                    overtaking = true;
+                    if (! turnLeft) turnLeft = distanceTravelled;
+                }
             }
 
-            if (overtaking) {
-                if (turnLeft) lastCommand.setAngle(-1);
+            if (overtaking) { // obstacle spotted, start turning left
+                if (turnLeft) {
+                    lastCommand.setAngle(-1);
 
-                if (imageProcessor::dashedLineGaps() > 1) {
-                    turnLeft = false;
-                    oomph = SensorData::encoderDistance();
+                    if (distanceTravelled - turnLeft > 50) { // turn left for 50 cm
+                        turnLeft = 0;
+
+                        if (! turnLeftCalibration) turnLeftCalibration = distanceTravelled;
+                    }
                 }
 
-                if (SensorData::encoderDistance() - oomph < 15) {
-                    lastCommand.setAngle(0);
+                if (turnLeftCalibration) {
+                    lastCommand.setAngle(1);
+
+                    if (distanceTravelled - turnLeftCalibration > 10) { // turn a bit to the right for 10cm to calibrate for easier lane following
+                        turnLeftCalibration = 0;
+                        turnRightCalibrationFinished = true;
+                    }
                 }
 
-                if (irRearRight > 5 && irRearRight < 10) if (! obstacleMet) obstacleMet = true;
+                if (turnRightCalibrationFinished) {
+                    if (irRearRight > 1 && irRearRight < 21) { // if irRearRight sees something it met the obstacle
+                        if (! obstacleMet) {
+                                turnRightCalibrationFinished = false;
+                                obstacleMet = true;
+                            }
+                    }
+                }
 
                 if (obstacleMet) {
-                    if (! irFrontRight) {
-                        obstacleMet = false;
-                        oomph = 0;
+                    if (! usFrontRight || usFrontRight > 20) { // if usFrontRight doesn't see anything, it passed the obstacle
+                        if (! obstaclePassed) {
+                            if (! turnRight) turnRight = distanceTravelled; // so start turning right
 
-                        if (! finaliseOvertaking) finaliseOvertaking = true;
-                        if (! turnRight) turnRight = true;
+                            obstaclePassed = true;
+                        }
+
+                    }
+
+                    if (turnRight) {
+                        lastCommand.setAngle(1);
+
+                        if (distanceTravelled - turnRight > 50) { // turn right for 50cm
+                            turnRight = 0;
+                            if (! turnRightCalibration) turnRightCalibration = distanceTravelled;
+                        }
+                    }
+
+                    if (turnRightCalibration) {
+                        lastCommand.setAngle(-1);
+
+                        if (distanceTravelled - turnRightCalibration > 10) { // turn a bit to the left for 10cm to calibrate for easier lane following
+                            turnRightCalibration = 0;
+                            obstacleMet = false;
+                            obstaclePassed = false;
+                            overtaking = false;
+                        }
                     }
                 }
+            }
 
-                if (finaliseOvertaking) {
-                    if (turnRight) lastCommand.setAngle(1);
+            if (debugMode) {
+                if (overtaking) {
+                    cv::putText(*mat, "overtaking", POINT(50.f, mat->size().height / 6.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+                }
 
-                    if (imageProcessor::dashedLineGaps() > 1) {
-                        turnRight = false;
-                        oomph = SensorData::encoderDistance();
-                    }
+                char turningLeftText[50];
+                sprintf(turningLeftText, "turning left: %d", turnLeft);
+                if (turnLeft) {
+                    cv::putText(*mat, turningLeftText, POINT(50.f, mat->size().height / 4.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+                }
 
-                    if (SensorData::encoderDistance() - oomph < 15) {
-                        lastCommand.setAngle(0);
-                    }
+                char turningRightText[50];
+                sprintf(turningRightText, "turning right: %d", turnRight);
+                if (turnRight) {
+                    cv::putText(*mat, turningRightText, POINT(50.f, mat->size().height / 4.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
+                }
 
-                    if (SensorData::encoderDistance() - oomph > 15) {
-                        turnRight = false;
-                        finaliseOvertaking = false;
-                        overtaking = false;
-                        oomph = 0;
-                    }
+                if (obstacleMet) {
+                    cv::putText(*mat, "obstacle met", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
                 }
             }
 
-            if (overtaking) {
-                cv::putText(*mat, "overtaking", POINT(50.f, mat->size().height / 4.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-            }
+            lastCommand.setSpeed(normalSpeed);
 
-            if (turnLeft) {
-                cv::putText(*mat, "turning left", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-            }
-
-            if (turnRight) {
-                cv::putText(*mat, "turning right", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-            }
-
-            if (oomph) {
-                cv::putText(*mat, "oomph", POINT(50.f, mat->size().height / 3.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-            }
-
-            if (obstacleMet) {
-                cv::putText(*mat, "oomph", POINT(50.f, mat->size().height / 2.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-            }
-
-            if (finaliseOvertaking) {
-                cv::putText(*mat, "finalise overtaking", POINT(50.f, mat->size().height / 1.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 2);
-            }
-
-            lastCommand.setSpeed(0.26);
             return lastCommand;
         }
     }
